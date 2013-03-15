34c866961a6f27e20531c5989da7790646546570
sj2903 <sj2903@mail.ru>
2013-03-15 16:07:27 +0300

    Added month validatin

--- 1.c
+++ 1.c
@@ -3,14 +3,14 @@
 #include <string.h>
 #include <malloc.h>
 
-/* enum month  {January, February, March, April, May, June, July, August, September, October, November, December};*/
-
 struct companys
 {
   char name[30];
   char tax[7];
-  char month_name[8];
-  char date[17];
+  char month_name[10];
+  int month_numb;
+  char rep_month_name[10];
+  int rep_month_numb;
 };
 struct tmp							// whether it  violates conditions of the task?
 {								
@@ -58,28 +58,51 @@ int scan_date(struct companys *company,int n)
   int i,j;
   for(i=0;i<n;i++)
   {
-    scanf_s("%s", company[i].month_name);
-    fflush(stdin);
+    company[i].month_numb = valid_input(company[i].month_name);	
   }
   for(i=0;i<n;i++)
   {
-    scanf_s("%s", company[i].date);
-    fflush(stdin);
+    company[i].rep_month_numb = valid_input(company[i].month_name);
   }
   return 0;  
 }
 
+int valid_input(char* month_name)
+{
+	int i,month_number;
+	char list_of_months[12][10]=
+	{"January","February","March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+	while(1)
+	{
+		scanf_s("%s", month_name);
+		fflush(stdin);
+		for(i=0;i<12;i++)
+		{
+			if ( (strcmp(list_of_months[i],month_name)) == 0 )
+			{
+				month_number=i;
+				break;
+				break;
+			}
+		}
+		printf("Invalid input");
+	}
+	return month_number;
+}
+
+
 int main()
 {
-  int j,i,n; 					// i,j - array steps, n - number of companys
+  int j,i,n,month_number,k=5; 					// i,j - array steps, n - number of companys
+  
   char month_name[8];	
   struct companys  *company,bf;				
   n=scan_names(company);
   scan_tax(company,n);
   scan_date(company,n);
   printf("Set date (month) to check companys with max debts");
-  gets(month_name);
- 
+  month_number=valid_input(month_name);
+
   return 0;
 }
-
