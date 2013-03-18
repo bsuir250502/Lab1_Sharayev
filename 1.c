0b49d089ca625eabc7016f14e0d447eb337fbb1c
sj2903 <sj2903@mail.ru>
2013-03-18 15:09:37 +0300

    Number of comp transferred as a pointer

--- 1.c
+++ 1.c
@@ -19,14 +19,14 @@ typedef struct
 }tmp;
 // Function uses temporary structure to scan names before the string starting from the point, and then allocates memory
 // for necessary number of names of type "struct companys" and rewrite data in allocated memory.
-companys* scan_names(int n)
+companys* scan_names(int* n)
 {
   int i,j;
   companys *comp;
   tmp *arr;
   arr=(tmp *)malloc(50 * sizeof(tmp) );
   printf("Set names of companys:\n");
-  for(i=0;i<n;i++) 
+  for(i=0;i<*n;i++) 
   {
   	fgets(arr[i].name,30,stdin);
   	fflush(stdin);
@@ -35,10 +35,10 @@ companys* scan_names(int n)
 		break;
   	}
   }
-  n=i;
-  comp=(companys *)malloc(n * sizeof(companys) );
+  *n=i;
+  comp=(companys *)malloc(*n * sizeof(companys) );
   
-  for(i=0;i<n;i++) 
+  for(i=0;i<*n;i++) 
   {
   	strcpy(comp[i].name,arr[i].name);
   }
@@ -101,22 +101,23 @@ int scan_dates(companys *comp,int n)
 
 int main()
 {
-  int j,i,n,month_number,better_numb; 					// i,j - array steps, n - number of companys
+  int j,i,*n,month_number,better_numb; 					// i,j - array steps, n - number of companys
   char month_name[10];	
-  companys  *comp,bf;				
+  companys  *comp,bf;	
+  n=(int *)malloc(sizeof(int));
  
-  n=50;
+  *n=50;
 
   comp=scan_names( n );
-  scan_tax( comp, n );
-  scan_dates( comp, n );
+  scan_tax( comp, *n );
+  scan_dates( comp, *n );
   
   printf("Set date (month) to check companys with max debts");
   month_number = valid_input(month_name);
   
   for(i=0;i<5;i++)
   {
-	for(j=i;j<n;j++)
+	for(j=i;j<*n;j++)
 	{
 		better_numb=i;
 		if( (comp[j].rep_month_numb<month_number) && (comp[better_numb].tax<comp[j].tax) )
