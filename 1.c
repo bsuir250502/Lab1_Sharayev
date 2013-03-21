ab90a67b0199264e1efa27c171c999cdeb9f7aec
sj2903 <sj2903@mail.ru>
2013-03-21 19:02:31 +0300

    Added scan of full date

--- 1.c
+++ 1.c
@@ -3,152 +3,155 @@
 #include <string.h>
 #include <malloc.h>
 
-typedef struct 
-{
-  char name[30];
-  char tax[8];
-  char month_name[10];
-  int  month_numb;
-  char rep_month_name[10];
-  int  rep_month_numb;
-}companys;
+typedef struct {
+    char name[30];
+    char tax[8];
+    int last_date;
+    int payment_date;
+} companies;
 
- typedef struct   						// whether it  violates conditions of the task?
-{								
-  char name[30];
-}tmp;
 // Function uses temporary structure to scan names before the string starting from the point, and then allocates memory
-// for necessary number of names of type "struct companys" and rewrite data in allocated memory.
-companys* scan_names(int* n)
+// for necessary number of names of type "struct companies" and rewrite data in allocated memory.
+companies *scan_names(int max, int *n)
 {
-  int i,j;
-  companys *comp;
-  tmp *arr;
-  arr=(tmp *)malloc(50 * sizeof(tmp) );
-  printf("Set names of companys:\n");
-  for(i=0;i<*n;i++) 
-  {
-  	fgets(arr[i].name,30,stdin);
-  	fflush(stdin);
-  	if(arr[i].name[0] == '.')
-  	{
-		break;
-  	}
-  }
-  *n=i;
-  comp=(companys *)malloc(*n * sizeof(companys) );
-  
-  for(i=0;i<*n;i++) 
-  {
-  	strcpy(comp[i].name,arr[i].name);
-  }
-  
-  free(arr);
-  return comp;
+    int i, j;
+    companies *comp;
+    char arr[50][30];
+    printf("Set names of companies:\n");
+    for (i = 0; i < max; i++) {
+        fgets(arr[i], 30, stdin);
+        if (arr[i][0] == '.') 
+        {
+            break;
+        }
+    }
+    *n = i;
+    comp = (companies *) malloc(*n * sizeof(companies));
+
+    for (i = 0; i < *n; i++) 
+    {
+        strncpy(comp[i].name, arr[i],30);
+    }
+
+    return comp;
 }
 
-int scan_tax(companys *comp, int n)
-{ 
-  int i,j;
-  printf("Set taxes %d:\n",n);
-  for(i=0;i<n;i++)
-  {
-  	fgets(comp[i].tax, 8, stdin);
-  	fflush(stdin);
-  }
-  return 0;
+int scan_tax(companies * comp, int n)
+{
+    int i, j;
+    printf("Set taxes %d:\n", n);
+    for (i = 0; i < n; i++) 
+    {
+        fgets(comp[i].tax, 8, stdin);
+    }
+    return 0;
 }
 
-int valid_input(char* month_name)
+int read_date(void)
 {
-  int i,month_number;
-  char list_of_months[][10]=
-  {"January","February","March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-  while(1)
-  {
-  	fgets(month_name,10,stdin);
-  	fflush(stdin);
-  	for(i=0;i<12;i++)
-  	{
-   		if ( (strcmp(list_of_months[i],month_name)) == 0 )
-   		{
-  			month_number=i;
-  			break;
-  			break;
-  		}
-  	}
-  	printf("Invalid input\n");
-  }
-  return month_number;
+    int i, month_number = 0;
+    char date[12], list_of_months[][4] =
+        { "JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL",
+"AUG", "SEP", "OCT", "NOV", "DEC" };
+    while (!month_number) 
+    {
+        fgets(date, 12, stdin);
+        for (i = 0; i < 12; i++)
+        {
+            if ((strstr(list_of_months[i], date)) != NULL) 
+            {
+                month_number = i+1;
+                break;
+            }
+            else
+            {
+                month_number = 0;
+            }
+        }
+        printf("Invalid input\n");
+    }
+	i=0;
+	while(date[i] != ' ') i++;
+	i++;
+
+	month_number+=(date[i++]-48)*10;
+	month_number+=(date[i++]-48);
+	i++;
+	month_number+=(date[i++]-48)*10000000;		// delicious solution
+	month_number+=(date[i++]-48)*1000000;
+	month_number+=(date[i++]-48)*100000;
+	month_number+=(date[i++]-48)*10000;
+
+    return month_number;
 }
 
 
-int scan_dates(companys *comp,int n)
+int scan_dates(companies * comp, int n)
 {
-  int i;
-  printf("Set the latest date for tax payment(month with a capital letter):\n");
-  for(i=0;i<n;i++)
-  {
-  	comp[i].month_numb = valid_input(comp[i].month_name);	
-  }
-  printf("Set the actual date for tax payment (month with a capital letter):\n");
-  for(i=0;i<n;i++)
-  {
-  	comp[i].rep_month_numb = valid_input(comp[i].month_name);
-  }
-  return 0;  
+    int i;
+    printf("Set the latest date for tax payment in American format(MMM DD YY):\n");
+    for (i = 0; i < n; i++) 
+    {
+        comp[i].last_date = read_date();
+    }
+    printf("Set the actual date for tax payment in American format(MMM DD YY):\n");
+    for (i = 0; i < n; i++) 
+    {
+        comp[i].payment_date = read_date();
+    }
+    return 0;
 }
 
 int main()
 {
-  int j,i,*n,month_number,better_numb; 					// i,j - array steps, n - number of companys
-  char month_name[10];	
-  companys  *comp,bf;	
-  n=(int *)malloc(sizeof(int));
- 
-  *n=50;
+    int j, i, n, month_number, better_numb;    // i,j - array steps, n - number of companies
+    char month_name[10];
+    companies *comp, bf;
 
-  comp=scan_names( n );
-  scan_tax( comp, *n );
-  scan_dates( comp, *n );
-  
-  printf("Set date (month) to check companys with max debts:\n");
-  month_number = valid_input(month_name);
-  
-  for(i=0;i<5;i++)
-  {
-	for(j=i;j<*n;j++)
-	{
-		better_numb=i;
-		if( (comp[j].rep_month_numb<month_number) && (comp[better_numb].tax<comp[j].tax) )
-		{
-			better_numb=j;
-		}
-	}
-	bf=comp[i];
-	comp[i]=comp[better_numb];
-	comp[better_numb]=bf;
-  }
-  for(i=0;i<5;i++)
-  {
-	for(j=i;j<5;j++)
-	{
-		better_numb=i;
-		if(comp[j].name[0]<comp[better_numb].name[0])
-		{
-			better_numb=j;
-		}
-	}
-	bf=comp[i];
-	comp[i]=comp[better_numb];
-	comp[better_numb]=bf;
-  }
+    n = 50;
 
-  printf("List of companies with the most outstanding tax before the %s:", month_name);
-  for(i=0;i<5;i++)
-  {
-	 printf("%d. %s %4s\n", i, comp[i].name, comp[i].tax);
-  }
+    comp = scan_names(n,&n);
+    scan_tax(comp, n);
+    scan_dates(comp, n);
 
-  return 0;
+    printf("Set date (month) to check companies with max debts:\n");
+    month_number = read_date();
+
+    for (i = 0; i < 5; i++) 
+    {
+        for (j = i; j < n; j++)
+        {
+            better_numb = i;
+            if ((comp[j].payment_date < month_number)
+                && (comp[better_numb].tax < comp[j].tax))
+            {
+                better_numb = j;
+            }
+        }
+        bf = comp[i];
+        comp[i] = comp[better_numb];
+        comp[better_numb] = bf;
+    }
+    for (i = 0; i < 5; i++) {
+        for (j = i; j < 5; j++)
+        {
+            better_numb = i;
+            if (comp[j].name[0] < comp[better_numb].name[0])
+            {
+                better_numb = j;
+            }
+        }
+        bf = comp[i];
+        comp[i] = comp[better_numb];
+        comp[better_numb] = bf;
+    }
+
+    printf
+        ("List of companies with the most outstanding tax before the %s:",
+         month_name);
+    for (i = 0; i < 5; i++) {
+        printf("%d. %s %4s\n", i, comp[i].name, comp[i].tax);
+    }
+
+    return 0;
 }
