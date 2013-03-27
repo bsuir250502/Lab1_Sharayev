4b80339389991d17d2a2011dd92a4f4a84f34feb
sj2903 <sj2903@mail.ru>
2013-03-27 16:23:24 +0300

    Removed cmp and myfgets bodies

--- 1.c
+++ 1.c
@@ -42,29 +42,6 @@ int main()
     return 0;
 }
 
-char* myfgets(char *str, int num)
-{
-    if( !(fgets(str, num, stdin)) ){
-    	return 0;
-    }
-    else {
-    	str[strlen(str)-1]='\0';
-    }
-    return str;
-}
-
-int cmp_tax(const void* a, const void* b)
-{
-    companies_t *ca=(companies_t *)a,*cb=(companies_t *)b;
-    return strcmp(cb->tax,ca->tax);
-}
-
-int cmp_name(const void* a, const void* b)
-{
-    companies_t *ca=(companies_t *)a,*cb=(companies_t *)b;
-    return ca->name[0]-cb->name[0];
-}
-
 int mysort(companies_t *comp, long int date ,int n)
 {
     int i,j = 0,num_of_debtrs;
