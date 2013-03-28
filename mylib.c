f9eca4204a37ebe5469ea4bc65c09cc5c0550b6b
sj2903 <sj2903@mail.ru>
2013-03-28 21:48:59 +0300

    Replaced read date

--- mylib.c
+++ mylib.c
@@ -1,24 +1,65 @@
 #include "mylib.h"
 
-char* myfgets(char *str, int num)
+char *myfgets(char *str, int num)
 {
-    if( !(fgets(str, num, stdin)) ){
-      return 0;
-    }
-    else {
-    	str[strlen(str)-1]='\0';
+    if (!(fgets(str, num, stdin))) {
+        return 0;
+    } else {
+        str[strlen(str) - 1] = '\0';
     }
     return str;
 }
 
-int cmp_tax(const void* a, const void* b)
+int input_number_in_range(int from, int to)
 {
-    companies_t *ca=(companies_t *)a,*cb=(companies_t *)b;
-    return strcmp(cb->tax,ca->tax);
+    int n;
+    char *endptr;
+    char input_buffer[128];
+    int first = 1;
+    do {
+        if(!first){
+            printf("Please enter number between %d and %d", from, to);
+        }
+        myfgets(input_buffer, 128);
+        n = strtol(input_buffer, &endptr, 10);
+        first = 0;
+    } while( *endptr || n <= from || n >= to );
+    return n;
 }
 
-int cmp_name(const void* a, const void* b)
+date_t read_date(void)
 {
-    companies_t *ca=(companies_t *)a,*cb=(companies_t *)b;
-    return ca->name[0]-cb->name[0];
+    char input_buffer[128];
+    int i, day, month, year;
+    long int date = 0;
+    char list_of_months[][4] =
+        { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
+        "Aug", "Sep", "Oct", "Nov", "Dec"
+    };
+
+    printf("  1)Specify the year(in format YYYY): ");
+    year = input_number_in_range(1970,2100);
+
+    printf("  3)Specify the month: ");
+    month = 0;
+    while (!month) {
+        myfgets(input_buffer, 12);
+        for (i = 0; i < 12; i++) {
+            if (!strncmp(input_buffer,list_of_months[i],3)) {
+                month = (i + 1);
+                break;
+            } else {
+                month = 0;
+            }
+        }
+        if (month == 0) {
+            printf("Invalid input, try again!\n");
+        }
+    }
+
+    printf("  3)Specify the day: ");
+    day = input_number_in_range(1,31);
+    date = year * 10000 + month * 100 + day;
+
+    return date;
 }
