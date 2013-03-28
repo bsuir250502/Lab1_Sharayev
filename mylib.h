6d812d4885b8775f2b9e6d0d63ea0d26536bdebf
sj2903 <sj2903@mail.ru>
2013-03-28 21:52:53 +0300

    Added some prototypes

--- mylib.h
+++ mylib.h
@@ -8,8 +8,7 @@
 typedef long int date_t;
 
 char* myfgets(char *, int);
-int cmp_tax(const void* , const void* );
-int cmp_name(const void* , const void*);
-
+int input_number_in_range(int from, int to);
+long int read_date(void);
 
 #endif
